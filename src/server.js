5e7703e61b1d6338f6b1d16d5fe9ded9c4a9d55d
“Kravid-Z” <kravidz64@gmail.com>
2021-04-15 00:31:09 +0200

    endpoints commented

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
-import listEndpoints from "express-list-endpoints";
+// import listEndpoints from "express-list-endpoints";
 import cors from "cors";
 import studentsRoutes from "./students/index.js";
 import projectsRoutes from "./projects/index.js";
 
 const server = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3001;
 
 server.use(cors()); // This is to avoid errors wotking in frontend and backend in same enviroment
 server.use(express.json());
@@ -13,7 +13,7 @@ server.use(express.json());
 server.use("/students", studentsRoutes);
 server.use("/projects", projectsRoutes);
 
-console.log(listEndpoints(server)); // this is for check it in real time with nodemon package in the console to get all routeslist
+// console.log(listEndpoints(server)); // this is for check it in real time with nodemon package in the console to get all routeslist
 
 server.listen(port, () => {
   console.log("Server is running on port ", port);
