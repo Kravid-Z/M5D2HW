e2c866e14d044e915c807026904d32d4bece55ae
“Kravid-Z” <kravidz64@gmail.com>
2021-04-14 15:43:08 +0200

    projects Route added

--- src/server.js
+++ src/server.js
@@ -2,14 +2,16 @@ import express from "express";
 import listEndpoints from "express-list-endpoints";
 import cors from "cors";
 import studentsRoutes from "./students/index.js";
+import projectsRoutes from "./projects/index.js";
 
 const server = express();
 const port = 3001;
 
 server.use(cors()); // This is to avoid errors wotking in frontend and backend in same enviroment
 server.use(express.json());
-server.listen();
+
 server.use("/students", studentsRoutes);
+server.use("/projects", projectsRoutes);
 
 console.log(listEndpoints(server)); // this is for check it in real time with nodemon package in the console to get all routeslist
 
